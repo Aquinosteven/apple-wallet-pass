fbd719c1535681737488cd81db5fb64f6605e9af
Aquinosteven <AQUINOSTEVEN@GMAIL.COM>
2025-12-17 19:17:30 -0700

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,12 +1,21 @@
+const ALLOWED = new Set([
+  "https://saas-wallet-pass-eve-iurc.bolt.host",
+  "http://localhost:5173",
+  "http://localhost:3000",
+]);
+
 export default function handler(req, res) {
-  // TEMP: allow any origin (we'll lock this down after Bolt connects)
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  const origin = req.headers.origin;
+
+  if (origin && ALLOWED.has(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Vary", "Origin");
+  }
+
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-  if (req.method === "OPTIONS") {
-    return res.status(204).end();
-  }
+  if (req.method === "OPTIONS") return res.status(204).end();
 
   return res.status(200).json({
     ok: true,
