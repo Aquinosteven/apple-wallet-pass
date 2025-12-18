bd868ebd3ff3a8caacb84cba06c98685888973f2
Aquinosteven <AQUINOSTEVEN@GMAIL.COM>
2025-12-17 19:20:02 -0700

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,17 +1,8 @@
-const ALLOWED = new Set([
-  "https://saas-wallet-pass-eve-iurc.bolt.host",
-  "http://localhost:5173",
-  "http://localhost:3000",
-]);
-
 export default function handler(req, res) {
-  const origin = req.headers.origin;
+  const origin = req.headers.origin || null;
 
-  if (origin && ALLOWED.has(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-    res.setHeader("Vary", "Origin");
-  }
-
+  // TEMP: allow all so we can diagnose
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
@@ -21,5 +12,6 @@ export default function handler(req, res) {
     ok: true,
     service: "apple-wallet-pass",
     time: new Date().toISOString(),
+    origin_received: origin,
   });
 }
